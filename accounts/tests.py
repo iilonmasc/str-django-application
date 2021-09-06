d132589408ec841ce5a7aa4f80f8195781287984
Tim Otlik <tim@otlik.de>
2021-09-06 10:26:28 +0200

    test: rename copied test name

--- accounts/tests.py
+++ accounts/tests.py
@@ -9,7 +9,7 @@ class UserAccountTestCase(TestCase):
     harry = {'first_name':'Harry', 'last_name':'Honest', 'iban':'DE68210501700012345678'}
     self.assertTrue(UserAccountForm(data=harry).is_valid())
 
-  def test_iban_is_validated(self):
+  def test_iban_is_not_validated(self):
     """Simon Shifty has an invalid IBAN, validation should fail"""
     simon = {'first_name':'Simon', 'last_name':'Shifty', 'iban':'DE69210501700012345678'}
     self.assertFalse(UserAccountForm(data=simon).is_valid())
