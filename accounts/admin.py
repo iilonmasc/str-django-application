49392172fabf347012c75e6e713a360a373a7615
Tim Otlik <tim@otlik.de>
2021-09-06 09:54:43 +0200

    test: add django.TestCase with unittest module
    Add Docstrings and reformat comments

--- accounts/admin.py
+++ accounts/admin.py
@@ -8,37 +8,50 @@ from .models import UserAccount
 # Register your models here.
 
 class UserAccountForm(forms.ModelForm):
+    """ModelForm needed for IBAN validation"""
     class Meta:
         model = UserAccount
         fields = ['first_name', 'last_name', 'iban']
 
-    def clean(self):
-        # remove whitespaces from "human readable" IBAN
-        iban = self.cleaned_data.get('iban').replace(' ', '')
+    def iban_is_valid(self, iban):
+        """Returns True if IBAN is valid
 
+        IBAN Structure:
+         - First two characters: ISO 3166 ALHPHA-2 country code
+         - Following two numbers: checksum
+         - Up to 30 characters: account number and bank clearing number
+
+        IBAN Validation:
+         1. Check if country code is valid
+         2. Convert each country code character to the corresponding integer position in the alphabet and add 9
+         3. Generate Checksum: Append converted country code and validation number to IBAN {IBAN}{Country Code}{Validation}
+         4. If IBAN Checksum % 97 == 1, the IBAN is valid
+        """
         country_code = iban[0:2].upper()
-        # check for ISO 3166 country codes
         if not pycountry.countries.get(alpha_2=country_code):
             raise forms.ValidationError("IBAN not valid(Country Code invalid)")
-        # check iban validation code
-        # - take country code and validation number
-        # - convert country code characters to integers (position in alphabet + 9)
         # ATTENTION: string.ascii_uppercase.index counts from 0, so use +10 instead of +9
-        # - append converted country code and validation to IBAN {IBAN}{Country Code}{Validation}
-        # - calculate result module 97
-        # - if modulo result is 1, IBAN is valid
         cc_integer = [str(string.ascii_uppercase.index(c)+10) for c in country_code]
+
         checksum = int(f'{iban[4:]}{"".join(cc_integer)}{iban[2:4]}')
-        
+
         if checksum % 97 != 1:
             raise forms.ValidationError("IBAN not valid(Validation calculation invalid)")
 
-        self.cleaned_data['iban'] = iban
+        return True
+
+    def clean(self):
+        """Clean form data, strip whitespaces from IBAN"""
+
+        iban = self.cleaned_data.get('iban').replace(' ', '')
+        if self.iban_is_valid(iban):
+            self.cleaned_data['iban'] = iban
         return self.cleaned_data
         
             
 
 class UserAccountAdmin(admin.ModelAdmin):
+    """ModelAdmin needed to display UserAccountForm in django-admin"""
     form = UserAccountForm
     fields = ['first_name', 'last_name', 'iban']
 
--- accounts/models.py
+++ accounts/models.py
@@ -3,12 +3,16 @@ from django.db import models
 # Create your models here.
 
 class UserAccount(models.Model):
+    """UserAccount model for a person with an IBAN
+       
+       Instance variables:
+       first_name -- First name of the person
+       last_name -- Last name of the person
+       iban -- IBAN of the person, max 34 characters according to ISO 13616-1:2020
+   """
     first_name = models.CharField(max_length=120)
     last_name = models.CharField(max_length=120)
     iban = models.CharField(max_length=34)
-    # IBAN validation:
-    # - 2 characters: for ISO 3166 ALHPHA-2 country code
-    # - 2 numbers: checksum
-    # - up to 30 characters: account number and bank clearing number
+
     def __str__(self):
         return f'{self.first_name} {self.last_name} - {self.iban}'
